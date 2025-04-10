96863e789ae6e6f68a731c0ca79614c289451d70
Alexhandru <alextitieni@yahoo.com>
2025-04-10 18:52:03 +0300

    Added more unit tests

--- test/SwapVolume.t.sol
+++ test/SwapVolume.t.sol
@@ -14,7 +14,7 @@ import {PoolSwapTest} from "v4-core/src/test/PoolSwapTest.sol";
 import {Counter} from "../src/Counter.sol";
 import {StateLibrary} from "v4-core/src/libraries/StateLibrary.sol";
 import {LPFeeLibrary} from "v4-core/src/libraries/LPFeeLibrary.sol";
-
+import {ProtocolFeeLibrary} from "v4-core/src/libraries/ProtocolFeeLibrary.sol";
 import {LiquidityAmounts} from "v4-core/test/utils/LiquidityAmounts.sol";
 import {IPositionManager} from "v4-periphery/src/interfaces/IPositionManager.sol";
 import {EasyPosm} from "./utils/EasyPosm.sol";
@@ -22,91 +22,140 @@ import {Fixtures} from "./utils/Fixtures.sol";
 import {SwapVolume} from "../src/SwapVolume.sol";
 
 contract SwapVolumeTest is Test, Fixtures {
-    using EasyPosm for IPositionManager;
-    using PoolIdLibrary for PoolKey;
-    using CurrencyLibrary for Currency;
     using StateLibrary for IPoolManager;
+    using ProtocolFeeLibrary for uint16;
 
     SwapVolume hook;
-    PoolId poolId;
 
-
-    uint256 tokenId;
-    int24 tickLower;
-    int24 tickUpper;
+    uint24 defaultFee = 3000;
+    uint24 feeAtMinAmount0 = 2700;
+    uint24 feeAtMaxAmount0 = 2400;
+    uint24 feeAtMinAmount1 = 2100;
+    uint24 feeAtMaxAmount1 = 2000;
+    uint256 minAmount0In = 1e18;
+    uint256 maxAmount0In = 10e18;
+    uint256 minAmount1In = 1e18;
+    uint256 maxAmount1In = 10e18;
 
     function setUp() public {
         deployFreshManagerAndRouters();
+
+        hook = SwapVolume(
+            address(uint160(Hooks.BEFORE_SWAP_FLAG))
+        );
+
+        deployCodeTo(
+            "SwapVolume.sol:SwapVolume",
+            abi.encode(
+                manager,
+                SwapVolume.SwapVolumeParams({
+                    defaultFee: defaultFee,
+                    feeAtMinAmount0: feeAtMinAmount0,
+                    feeAtMaxAmount0: feeAtMaxAmount0,
+                    feeAtMinAmount1: feeAtMinAmount1,
+                    feeAtMaxAmount1: feeAtMaxAmount1,
+                    minAmount0In: minAmount0In,
+                    maxAmount0In: maxAmount0In,
+                    minAmount1In: minAmount1In,
+                    maxAmount1In: maxAmount1In
+                })
+            ),
+            address(hook)
+        );
+
         deployMintAndApprove2Currencies();
-        deployAndApprovePosm(manager);
-
-        // Deploy the hook to an address with the correct flags
-        address flags = address(
-            uint160(Hooks.BEFORE_SWAP_FLAG) ^ (0x4444 << 144) // Namespace the hook to avoid collisions
-        );
-    
-
-        bytes memory constructorArgs = abi.encode(
-            manager,
-            SwapVolume.SwapVolumeParams({
-                defaultFee: 3000,
-                feeAtMinAmount0: 1000,
-                feeAtMaxAmount0: 990,
-                feeAtMinAmount1: 2700,
-                feeAtMaxAmount1: 2400,
-                minAmount0In: 1e18,
-                maxAmount0In: 10e18,
-                minAmount1In: 1e18,
-                maxAmount1In: 10e18
-            })
-        ); //Add all the necessary constructor arguments from the hook
-        deployCodeTo("SwapVolume.sol:SwapVolume", constructorArgs, flags);
-        hook = SwapVolume(flags);
-
-        // Create the pool
-        key = PoolKey(currency0, currency1, LPFeeLibrary.DYNAMIC_FEE_FLAG, 60, IHooks(hook));
-        poolId = key.toId();
-        manager.initialize(key, SQRT_PRICE_1_1);
-
-        // Provide full-range liquidity to the pool
-        tickLower = TickMath.minUsableTick(key.tickSpacing);
-        tickUpper = TickMath.maxUsableTick(key.tickSpacing);
-
-        uint128 liquidityAmount = 100e18;
-
-        (uint256 amount0Expected, uint256 amount1Expected) = LiquidityAmounts.getAmountsForLiquidity(
-            SQRT_PRICE_1_1,
-            TickMath.getSqrtPriceAtTick(tickLower),
-            TickMath.getSqrtPriceAtTick(tickUpper),
-            liquidityAmount
-        );
-
-        (tokenId,) = posm.mint(
-            key,
-            tickLower,
-            tickUpper,
-            liquidityAmount,
-            amount0Expected + 1,
-            amount1Expected + 1,
-            address(this),
-            block.timestamp,
-            ZERO_BYTES
+        (key,) = initPoolAndAddLiquidity(
+            currency0, currency1, IHooks(address(hook)), LPFeeLibrary.DYNAMIC_FEE_FLAG, SQRT_PRICE_1_1
         );
     }
 
-    function testSwapVolume() public {
+    function test_swap_updateDynamicFee_defaultFee() public {
+        bool zeroForOne = true;
+        int256 amountSpecified = -1e18 + 1; // Swap amount that doesn't hit min or max thresholds
+        swap(key, zeroForOne, amountSpecified, ZERO_BYTES);
+
+        // Check the swap amounts
+        assertEq(_fetchPoolLPFee(key), defaultFee);
+    }
+
+    function test_swap_updateDynamicFee_mintAmount0_feeAtMinAmount0() public {
         bool zeroForOne = true;
         int256 amountSpecified = -1e18; // negative number indicates exact input swap!
-        BalanceDelta delta = swap(key, zeroForOne, amountSpecified, ZERO_BYTES);
+        swap(key, zeroForOne, amountSpecified, ZERO_BYTES);
         
         // Check the swap amounts
-        assertEq(int256(delta.amount0()), int256(-1e18));
-        assertEq(int256(delta.amount1()), 1e18, "Incorrect amount in");
+        assertEq(_fetchPoolLPFee(key), feeAtMinAmount0);
+    }
 
-        // Check the fees collected
-        //(uint256 fee0, uint256 fee1) = manager.getFeesCollected(poolId);
-        
-        // assertEq(fee0, 0, "Incorrect fee0 collected");
-        // assertEq(fee1, 0, "Incorrect fee1 collected");
+    function test_swap_updateDynamicFee_mintAmount0_feeAtMaxAmount0() public {
+        bool zeroForOne = true;
+        int256 amountSpecified = -10e18; // Exact input swap hitting max threshold
+        swap(key, zeroForOne, amountSpecified, ZERO_BYTES);
+
+        // Check the swap amounts
+        assertEq(_fetchPoolLPFee(key), feeAtMaxAmount0);
+    }
+
+    function test_swap_updateDynamicFee_mintAmount1_feeAtMinAmount1() public {
+        bool zeroForOne = false;
+        int256 amountSpecified = -1e18; // Exact input swap hitting min threshold for token1
+        swap(key, zeroForOne, amountSpecified, ZERO_BYTES);
+
+        // Check the swap amounts
+        assertEq(_fetchPoolLPFee(key), feeAtMinAmount1);
+    }
+
+    function test_swap_updateDynamicFee_mintAmount1_feeAtMaxAmount1() public {
+        bool zeroForOne = false;
+        int256 amountSpecified = -10e18; // Exact input swap hitting max threshold for token1
+        swap(key, zeroForOne, amountSpecified, ZERO_BYTES);
+
+        // Check the swap amounts
+        assertEq(_fetchPoolLPFee(key), feeAtMaxAmount1);
+    }
+
+    function test_swap_middleVolume_exactInput_zeroForOne() public {
+        bool zeroForOne = true;
+        int256 amountSpecified = -int256((minAmount0In + maxAmount0In) / 2); // Dynamically calculate middle value
+        uint24 expectedFee = uint24(feeAtMinAmount0 - ((feeAtMinAmount0 - feeAtMaxAmount0) * ((minAmount0In + maxAmount0In) / 2 - minAmount0In)) / (maxAmount0In - minAmount0In));
+        swap(key, zeroForOne, amountSpecified, ZERO_BYTES);
+
+        // Check the swap amounts
+        assertEq(_fetchPoolLPFee(key), expectedFee);
+    }
+
+    function test_swap_middleVolume_exactOutput_zeroForOne() public {
+        bool zeroForOne = true;
+        int256 amountSpecified = int256((minAmount1In + maxAmount1In) / 2); // Dynamically calculate middle value
+        uint24 expectedFee = uint24(feeAtMinAmount1 - ((feeAtMinAmount1 - feeAtMaxAmount1) * ((minAmount1In + maxAmount1In) / 2 - minAmount1In)) / (maxAmount1In - minAmount1In));
+        swap(key, zeroForOne, amountSpecified, ZERO_BYTES);
+
+        // Check the swap amounts
+        assertEq(_fetchPoolLPFee(key), expectedFee);
+    }
+
+    function test_swap_middleVolume_exactInput_notZeroForOne() public {
+        bool zeroForOne = false;
+        int256 amountSpecified = -int256((minAmount1In + maxAmount1In) / 2); // Dynamically calculate middle value
+        uint24 expectedFee = uint24(feeAtMinAmount1 - ((feeAtMinAmount1 - feeAtMaxAmount1) * ((minAmount1In + maxAmount1In) / 2 - minAmount1In)) / (maxAmount1In - minAmount1In));
+        swap(key, zeroForOne, amountSpecified, ZERO_BYTES);
+
+        // Check the swap amounts
+        assertEq(_fetchPoolLPFee(key), expectedFee);
+    }
+
+    function test_swap_middleVolume_exactOutput_notZeroForOne() public {
+        bool zeroForOne = false;
+        int256 amountSpecified = int256((minAmount0In + maxAmount0In) / 2); // Dynamically calculate middle value
+        uint24 expectedFee = uint24(feeAtMinAmount0 - ((feeAtMinAmount0 - feeAtMaxAmount0) * ((minAmount0In + maxAmount0In) / 2 - minAmount0In)) / (maxAmount0In - minAmount0In));
+        swap(key, zeroForOne, amountSpecified, ZERO_BYTES);
+
+        // Check the swap amounts
+        assertEq(_fetchPoolLPFee(key), expectedFee);
+    }
+
+    function _fetchPoolLPFee(PoolKey memory _key) internal view returns (uint256 lpFee) {
+        PoolId id = _key.toId();
+        (,,, lpFee) = manager.getSlot0(id);
     }
 }
